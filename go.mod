0745bb46469ca109d97ba7579a23b2751ee826fc
zhangweisong <zhangweisong@mixlinker.com>
2023-01-29 16:16:44 +0800

    完成

--- go.mod
+++ go.mod
@@ -6,9 +6,9 @@ replace origin => ./origin
 
 replace model => ./model
 
-require origin v0.0.0-00010101000000-000000000000
+replace dbsql => ./dbsql
 
 require (
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	model v0.0.0-00010101000000-000000000000 // indirect
+	github.com/go-sql-driver/mysql v1.7.0
+	model v0.0.0-00010101000000-000000000000
 )
--- main.go
+++ main.go
@@ -1,37 +1,98 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"model"
-	"origin"
 )
 
+var db *sql.DB
+
 func main() {
-	db, err := origin.Connect("recordings")
+	var err error
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/recordings")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connected!")
-	lists, err := db.Select("SELECT * FROM album WHERE artist = ?", "John Coltrane")
-	if nil != err {
-		log.Fatal(err)
-	}
-	fmt.Printf("Albums found: %v\n", lists)
 
-	newalbum, newerr := db.ItemByID(2)
-	if newerr != nil {
-		log.Fatal(newerr)
-	}
-	fmt.Printf("newalbum found: %v\n", newalbum)
-
-	albID, err := db.Add(model.Album{
-		Title:  "The Modern Sound of Betty Carter",
-		Artist: "Betty Carter",
-		Price:  49.99,
-	})
+	list, err := AlbumByID(1)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("ID of added album: %v\n", albID)
+	fmt.Println(list)
+
+}
+
+func AddAlbum(alb model.Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist,price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("AddAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("AddAlbum: %v", err)
+	}
+	return id, nil
+}
+
+func canPurchase(id int, price int) (bool, error) {
+	var enough bool
+	// 基于单行查询值. 因为查询的只有一个字段，最终结果是1*1 的 放入一个变量即可。
+	if err := db.QueryRow("SELECT (price >= ?) from album where id = ?", price, id).Scan(&enough); err != nil {
+		if err == sql.ErrNoRows {
+			return false, fmt.Errorf("canPurchase %d: unknown album", id)
+		}
+		return false, fmt.Errorf("canPurchase %d: %v", id)
+	}
+	return enough, nil
+}
+
+func albumsByArtist(artist string) ([]model.Album, error) {
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", artist)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// 用于保存返回行中的数据的专辑切片
+	var albums []model.Album
+
+	// 遍历行，使用Scan将列数据分配给结构字段。
+	for rows.Next() {
+		var alb model.Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist,
+			&alb.Price); err != nil {
+			return albums, err
+		}
+		albums = append(albums, alb)
+	}
+	if err = rows.Err(); err != nil {
+		return albums, err
+	}
+	return albums, nil
+}
+
+// AlbumByID 检索指定的专辑.
+func AlbumByID(id int) (model.Album, error) {
+	// 定义预处理语句。您通常会在其他位置定义语句，
+	// 并将其保存以用于诸如此函数的函数中
+	stmt, err := db.Prepare("SELECT * FROM album WHERE id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var album model.Album
+
+	// 执行预准备语句，为占位符为 ？ 的
+	// 参数传入 id 值
+	err = stmt.QueryRow(id).Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// 处理未返回行的情况.
+		}
+		return album, err
+	}
+	return album, nil
 }
